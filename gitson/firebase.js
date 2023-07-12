70b4beeb382668ddae64387198b8b3e240663763
Harshilsh <115207214+Harshilsh@users.noreply.github.com>
2023-07-12 14:56:16 +0530

    Add files via upload

---
+++ gitson/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDUdoYHSj--KrKkrFkueedQt6_GRdD3xoQ",
+    authDomain: "gitson-website.firebaseapp.com",
+    projectId: "gitson-website",
+    storageBucket: "gitson-website.appspot.com",
+    messagingSenderId: "766534107193",
+    appId: "1:766534107193:web:2fe56971d463429e554de8",
+    measurementId: "G-B1FPQL6YHQ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
